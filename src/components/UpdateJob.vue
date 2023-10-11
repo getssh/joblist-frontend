18d39104103b01921ba32c5fd19468ad1807fbea
getssh <gtamrat33@gmail.com>
2023-10-12 01:38:14 +0300

    Add all feilds to update and add header auth

--- src/components/UpdateJob.vue
+++ src/components/UpdateJob.vue
@@ -8,18 +8,44 @@
       </div>
       <div class="form-group">
         <label for="company">Logo URL</label>
-        <input v-model="editedJob.logo" type="text" id="company" required />
+        <input v-model="editedJob.logo" type="text" id="logo" required />
       </div>
       <div class="form-group">
-        <label for="company">Company:</label>
-        <input v-model="editedJob.company" type="text" id="company" required />
-      </div>
-      <div class="form-group">
-        <label for="company">Company:</label>
-        <input v-model="editedJob.company" type="text" id="company" required />
-      </div>
-      
-      
+          <label for="new">New</label>
+          <input v-model="job.new" type="checkbox" id="new" />
+        </div>
+        <div class="form-group">
+          <label for="featured">Featured</label>
+          <input v-model="editedJob.featured" type="checkbox" id="featured" />
+        </div>
+        <div class="form-group">
+          <label for="position">Position</label>
+          <input v-model="editedJob.position" type="text" id="position" required />
+        </div>
+        <div class="form-group">
+          <label for="role">Role</label>
+          <input v-model="editedJob.role" type="text" id="role" required />
+        </div>
+        <div class="form-group">
+          <label for="level">Level</label>
+          <input v-model="editedJob.level" type="text" id="level" required />
+        </div>
+        <div class="form-group">
+          <label for="contract">Contract</label>
+          <input v-model="editedJob.contract" type="text" id="contract" required />
+        </div>
+        <div class="form-group">
+          <label for="location">Location</label>
+          <input v-model="editedJob.location" type="text" id="location" required />
+        </div>
+        <div class="form-group">
+          <label for="languages">Languages</label>
+          <input v-model="editedJob.languages" type="text" id="languages" required />
+        </div>
+        <div class="form-group">
+          <label for="tools">Tools</label>
+          <input v-model="editedJob.tools" type="text" id="tools" required />
+        </div>
       <button type="submit">Update Job</button>
     </form>
   </div>
@@ -27,6 +53,7 @@
 
 <script>
 import axios from 'axios';
+import { useAuthStore } from '@/store/auth';
 
 export default {
   data() {
@@ -37,8 +64,16 @@ export default {
   },
   methods: {
     updateJob() {
+      const authStore = useAuthStore();
+      const userToken = authStore.token;
+
       axios
-        .put(`http://localhost:5000/api/jobs/${this.$route.params.id}`, this.editedJob)
+        .put(`http://localhost:5000/api/jobs/${this.$route.params.id}`, this.editedJob, {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+            'Content-Type': 'application/json',
+          }
+        })
         .then((response) => {
           if (response.data) {
             this.jobUpdateMessage = 'Job updated successfully';
